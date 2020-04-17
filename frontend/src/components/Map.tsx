b9604f911e99b9a80b73e96c07617f7777288678
srafi1 <shakilrafi0@gmail.com>
2020-04-17 01:37:00 -0400

    map with disabled controls

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -5,10 +5,11 @@ import { compose, withProps } from 'recompose';
 import Loading from './Loading';
 
 const Map = compose(
+  // this api key is locked to *.shakilrafi.com
   withProps({
     googleMapURL:
       "https://maps.googleapis.com/maps/api/js?key=AIzaSyAC4hh69r1rQ-rCtDZ8muFywZQFKp3_ZsM&v=3.exp",
-    loadingElement: Loading,
+    loadingElement: <Loading />,
     containerElement: <Box height={1} />,
     mapElement: <Box height={1} />
   }),
@@ -16,8 +17,14 @@ const Map = compose(
   withGoogleMap
 )(props => (
   <GoogleMap
-    defaultZoom={20}
-    defaultCenter={{ lat: 40.76784, lng: -73.963901 }} />
+    zoom={20}
+    defaultCenter={{ lat: 40.76784, lng: -73.963901 }}
+    clickableIcons={false}
+    options={{
+      keyboardShortcuts: false,
+      disableDefaultUI: true,
+      gestureHandling: 'none'
+    }} />
 ));
 
 export default Map;
