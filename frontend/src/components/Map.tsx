a6b1daed2daaaace229f2fbf3ef98de03704f7aa
srafi1 <shakilrafi0@gmail.com>
2020-04-17 01:13:38 -0400

    finally added map

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Box, ThemeProvider } from '@material-ui/core';
-import theme from '../common/theme';
+import { Box } from '@material-ui/core';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import { compose, withProps } from 'recompose';
+import Loading from './Loading';
 
-function Map() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Box
-        bgcolor="background.default"
-        color="text.primary"
-        height={1}>
-      </Box>
-    </ThemeProvider>
-  );
-}
+const Map = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAC4hh69r1rQ-rCtDZ8muFywZQFKp3_ZsM&v=3.exp",
+    loadingElement: Loading,
+    containerElement: <Box height={1} />,
+    mapElement: <Box height={1} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap
+    defaultZoom={20}
+    defaultCenter={{ lat: 40.76784, lng: -73.963901 }} />
+));
 
 export default Map;
