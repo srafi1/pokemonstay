8175454b68aa73b05425393e68a60f8c8f83a68e
srafi1 <shakilrafi0@gmail.com>
2020-04-20 18:29:02 -0400

    embed coords into spawn

--- backend/spawn/spawn.go
+++ backend/spawn/spawn.go
@@ -10,26 +10,31 @@ type Coords struct {
 }
 
 type Spawn struct {
-    Lat float64 `json:"lat"`
-    Lng float64 `json:"lng"`
+    *Coords
     Dex int16 `json:"dex"`
 }
 
 func GetSpawns(lat float64, lng float64) []Spawn {
     spawns := []Spawn{}
     spawns = append(spawns, Spawn{
-        Lat: lat,
-        Lng: lng - 0.0001,
+        Coords: &Coords{
+            Lat: lat,
+            Lng: lng - 0.0001,
+        },
         Dex: 1,
     })
     spawns = append(spawns, Spawn{
-        Lat: lat,
-        Lng: lng,
+        Coords: &Coords{
+            Lat: lat,
+            Lng: lng,
+        },
         Dex: 2,
     })
     spawns = append(spawns, Spawn{
-        Lat: lat,
-        Lng: lng + 0.0001,
+        Coords: &Coords{
+            Lat: lat,
+            Lng: lng + 0.0001,
+        },
         Dex: 3,
     })
     return spawns
