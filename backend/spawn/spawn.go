3010d7d8bd6749b2c78855bc28364a030f6ed63d
srafi1 <shakilrafi0@gmail.com>
2020-04-26 17:40:28 -0400

    use quadtree to manage spawned pokemon

--- backend/spawn/spawn.go
+++ backend/spawn/spawn.go
@@ -1,114 +1,128 @@
 package spawn
 
 import (
-    "math/rand"
-    "sync"
-    "time"
+    "math"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 var pokemonQueue []Spawn
+var globalQuadTree *QuadTree
 var userLocs map[string]Coords
 var spawnLock sync.RWMutex
 var userEncounters map[string]map[Spawn]bool
+
 const MAX_DEX = 807
+const SPAWN_RANGE = 0.003
+const MAX_IN_AREA = 50
 
 type Coords struct {
-    Lat float64 `json:"lat"`
-    Lng float64 `json:"lng"`
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
 }
 
 type Spawn struct {
-    Coords
-    Dex int `json:"dex"`
-    Despawn time.Time
+	Coords
+	Dex     int `json:"dex"`
+	Despawn time.Time
 }
 
 func Init() {
-    pokemonQueue = make([]Spawn, 0)
-    userLocs = make(map[string]Coords, 0)
-    spawnLock = sync.RWMutex{}
-    userEncounters = make(map[string]map[Spawn]bool)
-    initRarities()
-    rand.Seed(time.Now().UnixNano())
-    go func() {
-        for {
-            spawnLock.Lock()
-            // despawn old pokemon
-            cutoff := 0
-            now := time.Now()
-            for cutoff < len(pokemonQueue) && now.After(pokemonQueue[cutoff].Despawn) {
-                cutoff++
-            }
-            pokemonQueue = pokemonQueue[cutoff:]
+	pokemonQueue = make([]Spawn, 0)
+	globalQuadTree = NewQuadTree()
+	userLocs = make(map[string]Coords, 0)
+	spawnLock = sync.RWMutex{}
+	userEncounters = make(map[string]map[Spawn]bool)
+	initRarities()
+	rand.Seed(time.Now().UnixNano())
 
-            // spawn new pokemon per user
-            despawnTime := time.Now().Add(time.Minute)
-            for _, loc := range userLocs {
-                pokemon := make([]Spawn, 10)
-                for i:= 0; i < 10; i++ {
-                    p := Spawn{
-                        Coords: loc,
-                        Dex: randDex(),
-                        Despawn: despawnTime,
-                    }
-                    p.Lat -= (rand.Float64() * 2 - 1) / 200
-                    p.Lng -= (rand.Float64() * 2 - 1) / 200
-                    pokemon[i] = p
-                }
-                pokemonQueue = append(pokemonQueue, pokemon...)
+	go func() {
+		for {
+			// despawn old pokemon
+			spawnLock.Lock()
+			cutoff := 0
+			now := time.Now()
+			for cutoff < len(pokemonQueue) && now.After(pokemonQueue[cutoff].Despawn) {
+				globalQuadTree.Remove(pokemonQueue[cutoff])
+				cutoff++
+			}
+			pokemonQueue = pokemonQueue[cutoff:]
+            spawnLock.Unlock()
+
+			// spawn new pokemon per user
+			despawnTime := time.Now().Add(time.Minute)
+            toSpawn := make([]Spawn, 0)
+			for _, loc := range userLocs {
+                numLocal := len(GetSpawns(loc))
+                maxNew := int(math.Max(1, float64(MAX_IN_AREA - numLocal)))
+                numNew := rand.Int() % maxNew
+				pokemon := make([]Spawn, numNew)
+				for i := 0; i < numNew; i++ {
+					p := Spawn{
+						Coords:  loc,
+						Dex:     randDex(),
+						Despawn: despawnTime,
+					}
+					p.Lat -= (rand.Float64()*2 - 1) * SPAWN_RANGE
+					p.Lng -= (rand.Float64()*2 - 1) * SPAWN_RANGE
+					pokemon[i] = p
+				}
+                toSpawn = append(toSpawn, pokemon...)
+			}
+
+            spawnLock.Lock()
+            pokemonQueue = append(pokemonQueue, toSpawn...)
+            for _, p := range toSpawn {
+                globalQuadTree.Add(p)
             }
             spawnLock.Unlock()
-            time.Sleep(5*time.Second)
+
+            time.Sleep(10*time.Second)
         }
     }()
 }
 
-func SpawnPokemon(pokemon []Spawn) {
-    spawnLock.Lock()
-    pokemonQueue = append(pokemonQueue, pokemon...)
-    spawnLock.Unlock()
-}
-
-func GetSpawns(coords Coords) []Spawn {
-    spawnLock.RLock()
-    ret := make([]Spawn, len(pokemonQueue))
-    copy(ret, pokemonQueue)
-    spawnLock.RUnlock()
-    return ret
+func GetSpawns(c Coords) []Spawn {
+	spawnLock.RLock()
+	ret := globalQuadTree.GetInRange(Bounds{
+		MinLat: c.Lat - SPAWN_RANGE,
+		MaxLat: c.Lat + SPAWN_RANGE,
+		MinLng: c.Lng - SPAWN_RANGE,
+		MaxLng: c.Lng + SPAWN_RANGE,
+	})
+	spawnLock.RUnlock()
+	return ret
 }
 
 func PutUser(user string, loc Coords) {
-    spawnLock.Lock()
-    userLocs[user] = loc
-    spawnLock.Unlock()
-    if _, ok := userEncounters[user]; !ok {
-        userEncounters[user] = make(map[Spawn]bool)
-    }
+	userLocs[user] = loc
+	if _, ok := userEncounters[user]; !ok {
+		userEncounters[user] = make(map[Spawn]bool)
+	}
 }
 
 func RemoveUser(user string) {
-    spawnLock.Lock()
-    delete(userLocs, user)
-    spawnLock.Unlock()
+	delete(userLocs, user)
 }
 
 func AddEncounter(user string, pokemon Spawn) {
-    userEncounters[user][pokemon] = true
+	userEncounters[user][pokemon] = true
 }
 
 func GetEncounters(user string) map[Spawn]bool {
-    encounters, _ := userEncounters[user]
-    return encounters
+	encounters, _ := userEncounters[user]
+	return encounters
 }
 
 func CleanupEncounters(user string) {
-    encounters, ok := userEncounters[user]
-    if ok {
-        now := time.Now()
-        for k := range encounters {
-            if now.After(k.Despawn) {
-                delete(encounters, k)
-            }
-        }
-    }
+	encounters, ok := userEncounters[user]
+	if ok {
+		now := time.Now()
+		for k := range encounters {
+			if now.After(k.Despawn) {
+				delete(encounters, k)
+			}
+		}
+	}
 }
