59d00a51e42613bd5742a2dc6b0619cc91701b41
srafi1 <shakilrafi0@gmail.com>
2020-05-27 16:11:26 -0400

    pokedex progress as text

--- frontend/src/components/Pokedex.tsx
+++ frontend/src/components/Pokedex.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, ThemeProvider, Typography } from '@material-ui/core';
 import theme from '../common/theme';
 import { Link } from 'react-router-dom';
@@ -7,6 +7,31 @@ import { commonStyles } from '../common/styles';
 
 function Pokedex() {
   const styles = commonStyles();
+  const [loaded, setLoaded] = useState(false);
+  const [pokedex, setPokedex] = useState([{
+    encountered: false,
+    caught: false,
+  }]);
+
+  if (!loaded) {
+    fetch('/api/pokedex', {
+      credentials: 'same-origin',
+    })
+    .then((res: Response) => {
+      if (res.status !== 200) {
+        console.log('Failed to retrieve pokedex');
+      } else {
+        res.json().then((json: any[]) => {
+          setPokedex(json);
+        });
+      }
+      setLoaded(true);
+    });
+  }
+
+  const totalEnc = pokedex.filter(a => a.encountered).length
+  const totalCaught = pokedex.filter(a => a.caught).length
+
   return (
     <ThemeProvider theme={theme}>
       <Box
@@ -20,6 +45,12 @@ function Pokedex() {
         <img src={logo} className={styles.logo} alt="Pokemon Stay" />
         <Typography variant="h3">
           pokedex page
+        </Typography>
+        <Typography variant="body1">
+          {totalEnc} / {pokedex.length} encountered
+        </Typography>
+        <Typography variant="body1">
+          {totalCaught} / {pokedex.length} caught
         </Typography>
         <Button 
           variant="contained" 
