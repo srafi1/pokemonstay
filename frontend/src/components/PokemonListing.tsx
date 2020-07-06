f84046825840a686ee404f0ae872f7c590d83ca7
srafi1 <shakilrafi0@gmail.com>
2020-07-06 19:53:41 -0400

    clean up image source

--- frontend/src/components/PokemonListing.tsx
+++ frontend/src/components/PokemonListing.tsx
@@ -12,18 +12,17 @@ function PokemonListing(props: {
   hidden: boolean,
   name: string,
 }) {
+  let imgSrc;
+  if (props.hidden) {
+    imgSrc = "/api/sprite?dex=0";
+  } else if (props.silhouette) {
+    imgSrc = `/api/sprite?dex=${props.dex}&silhouette`;
+  } else {
+    imgSrc = `/api/sprite?dex=${props.dex}`;
+  }
   return (
     <Box p={1} m={1} border="1px solid white" borderRadius={10}>
-      {props.hidden ?
-        <img src="/api/sprite?dex=0" width={200} alt="0" /> :
-        props.silhouette ?
-          <img
-            src={`/api/sprite?dex=${props.dex}&silhouette`}
-            width={200} alt={`${props.dex}`} /> :
-              <img
-                src={`/api/sprite?dex=${props.dex}`}
-                width={200} alt={`${props.dex}`} />
-      }
+      <img src={imgSrc} width={200} alt="0" />
       <Typography variant="body1" align="center">
         {props.showDex && `${props.dex} `}
         {capitalize(`${props.name}`)}
